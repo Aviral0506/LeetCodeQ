d3d5ddb985847e1c06cad0754621be07084248bd
Aviral Chaurasia <152956186+Aviral0506@users.noreply.github.com>
2026-06-22 09:46:12 +0530

    Time: 1 ms (17.57%) | Memory: 23.9 MB (55.10%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int slow = 0;
         int n = nums.size();
+        int slow = 0;
         for(int fast = 0; fast < n; fast++){
             if(nums[fast] != 0){
                 nums[slow++] = nums[fast];
